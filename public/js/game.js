96d997e5b457afc7076052dcbe9ee7c67c2fba38
makdenis <makdenis.1997@gmail.com>
2018-09-16 21:50:09 +0300

    validation

--- public/js/game.js
+++ public/js/game.js
@@ -3,379 +3,411 @@
 const game = document.getElementById('game');
 
 
-function ajax (callback, method, path, body) {
-	const xhr = new XMLHttpRequest();
-	xhr.open(method, path, true);
-	xhr.withCredentials = true;
+function ajax(callback, method, path, body) {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, path, true);
+    xhr.withCredentials = true;
 
-	if (body) {
-		xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
-	}
+    if (body) {
+        xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+    }
 
-	xhr.onreadystatechange = function () {
-		if (xhr.readyState !== 4) {
-			return;
-		}
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState !== 4) {
+            return;
+        }
 
-		callback(xhr);
-	};
+        callback(xhr);
+    };
 
-	if (body) {
-		xhr.send(JSON.stringify(body));
-	} else {
-		xhr.send();
-	}
+    if (body) {
+        xhr.send(JSON.stringify(body));
+    } else {
+        xhr.send();
+    }
 }
 
-function createMenuLink () {
-	const menuLink = document.createElement('a');
-	menuLink.href = menuLink.dataset.href = 'menu';
+function createMenuLink() {
+    const menuLink = document.createElement('a');
+    menuLink.href = menuLink.dataset.href = 'menu';
 
-	menuLink.textContent = 'Back to main menu';
+    menuLink.textContent = 'Back to main menu';
 
-	return menuLink;
+    return menuLink;
 }
 
-function createMenu () {
-	const menuSection = document.createElement('section');
-	menuSection.dataset.sectionName = 'menu';
-	menuSection.id = 'mainMenu'
+function createMenu() {
+    const menuSection = document.createElement('section');
+    menuSection.dataset.sectionName = 'menu';
+    menuSection.id = 'mainMenu'
 
-	const header = document.createElement('div');
-	header.id = 'header';
+    const header = document.createElement('div');
+    header.id = 'header';
 
-	const logo = document.createElement('div');
-	logo.id = 'logo';
-	const logoHeader = document.createElement('h1');
-	logoHeader.textContent = 'Yet Another Game';
+    const logo = document.createElement('div');
+    logo.id = 'logo';
+    const logoHeader = document.createElement('h1');
+    logoHeader.textContent = 'Yet Another Game';
 
-	logo.appendChild(logoHeader);
-
-
-	const main = document.createElement('div');
-	main.id = 'main';
-	const mainInner = document.createElement('div');
-
-	main.appendChild(mainInner);
-
-	const register = {
-		sign_in: 'Sign in',
-		sign_up: 'Sign up',
-	};
-
-	const titles = {
-		new_game: 'New Game',
-		leaders: 'Leaderboard',
-		me: 'Profile'
-	};
+    logo.appendChild(logoHeader);
 
 
-	Object.entries(register).forEach(function (elem) {
-		const href = elem[ 0 ];
-		const title = elem[ 1 ];
+    const main = document.createElement('div');
+    main.id = 'main';
+    const mainInner = document.createElement('div');
 
-		const a = document.createElement('a');
-		a.href = href;
-		a.dataset.href = href;
-		a.textContent = title;
-		a.classList.add('header-button');
+    main.appendChild(mainInner);
 
-		header.appendChild(a);
-	});
+    const register = {
+        sign_in: 'Sign in',
+        sign_up: 'Sign up',
+    };
 
-	Object.entries(titles).forEach(function (entry) {
-		const href = entry[ 0 ];
-		const title = entry[ 1 ];
+    const titles = {
+        new_game: 'New Game',
+        leaders: 'Leaderboard',
+        me: 'Profile'
+    };
 
-		const a = document.createElement('a');
-		a.href = href;
-		a.dataset.href = href;
-		a.textContent = title;
-		a.classList.add('menu-button');
 
-		mainInner.appendChild(a);
-	});
+    Object.entries(register).forEach(function (elem) {
+        const href = elem[0];
+        const title = elem[1];
 
-	menuSection.appendChild(header);
-	menuSection.appendChild(logo);
-	menuSection.appendChild(main);
+        const a = document.createElement('a');
+        a.href = href;
+        a.dataset.href = href;
+        a.textContent = title;
+        a.classList.add('header-button');
 
-	game.appendChild(menuSection);
+        header.appendChild(a);
+    });
+
+    Object.entries(titles).forEach(function (entry) {
+        const href = entry[0];
+        const title = entry[1];
+
+        const a = document.createElement('a');
+        a.href = href;
+        a.dataset.href = href;
+        a.textContent = title;
+        a.classList.add('menu-button');
+
+        mainInner.appendChild(a);
+    });
+
+    menuSection.appendChild(header);
+    menuSection.appendChild(logo);
+    menuSection.appendChild(main);
+
+    game.appendChild(menuSection);
 }
 
-function createSignIn () {
-	const signInSection = document.createElement('section');
-	signInSection.dataset.sectionName = 'sign_in';
+function createSignIn() {
+    const signInSection = document.createElement('section');
+    signInSection.dataset.sectionName = 'sign_in';
 
-	const header = document.createElement('h1');
-	header.textContent = 'Sign In';
+    const header = document.createElement('h1');
+    header.textContent = 'Sign In';
 
 
-	const form = document.createElement('form');
+    const form = document.createElement('form');
 
-	const inputs = [
-		{
-			name: 'email',
-			type: 'email',
-			placeholder: 'Email'
-		},
-		{
-			name: 'password',
-			type: 'password',
-			placeholder: 'Password'
-		},
-		{
-			name: 'submit',
-			type: 'submit'
-		}
-	];
+    const inputs = [
+        {
+            name: 'email',
+            type: 'email',
+            placeholder: 'Email'
+        },
+        {
+            name: 'password',
+            type: 'password',
+            placeholder: 'Password'
+        },
+        {
+            name: 'submit',
+            type: 'submit'
+        }
+    ];
 
-	inputs.forEach(function (item) {
-		const input = document.createElement('input');
+    inputs.forEach(function (item) {
+        const input = document.createElement('input');
 
-		input.name = item.name;
-		input.type = item.type;
+        input.name = item.name;
+        input.type = item.type;
 
-		input.placeholder = item.placeholder;
+        input.placeholder = item.placeholder;
 
-		form.appendChild(input);
-		form.appendChild(document.createElement('br'));
-	});
+        form.appendChild(input);
+        form.appendChild(document.createElement('br'));
+    });
 
-	signInSection.appendChild(header);
-	signInSection.appendChild(form);
-	signInSection.appendChild(createMenuLink());
+    signInSection.appendChild(header);
+    signInSection.appendChild(form);
+    signInSection.appendChild(createMenuLink());
 
-	form.addEventListener('submit', function (event) {
-		event.preventDefault();
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
 
-		const email = form.elements[ 'email' ].value;
-		const password = form.elements[ 'password' ].value;
+        const email = form.elements['email'].value;
+        const password = form.elements['password'].value;
 
-		ajax(function (xhr) {
-			game.innerHTML = '';
-			createProfile();
-		}, 'POST', '/login', {
-			email: email,
-			password: password
-		});
-	});
+        ajax(function (xhr) {
+            game.innerHTML = '';
+            createProfile();
+        }, 'POST', '/login', {
+            email: email,
+            password: password
+        });
+    });
 
-	game.appendChild(signInSection);
+    game.appendChild(signInSection);
 }
 
-function createSignUp () {
-	const signUpSection = document.createElement('section');
-	signUpSection.dataset.sectionName = 'sign_in';
+function createSignUp() {
+    const signUpSection = document.createElement('section');
+    signUpSection.dataset.sectionName = 'sign_in';
 
-	const header = document.createElement('h1');
-	header.textContent = 'Sign Up';
+    const header = document.createElement('h1');
+    header.textContent = 'Sign Up';
 
 
-	const form = document.createElement('form');
+    const form = document.createElement('form');
 
-	const inputs = [
-		{
-			name: 'email',
-			type: 'email',
-			placeholder: 'Email'
-		},
-		{
-			name: 'age',
-			type: 'number',
-			placeholder: 'Your Age'
-		},
-		{
-			name: 'password',
-			type: 'password',
-			placeholder: 'Password'
-		},
-		{
-			name: 'password_repeat',
-			type: 'password',
-			placeholder: 'Repeat Password'
-		},
-		{
-			name: 'submit',
-			type: 'submit'
-		}
-	];
+    const inputs = [
+        {
+            name: 'email',
+            type: 'email',
+            placeholder: 'Email'
+        },
+        {
+            name: 'age',
+            type: 'number',
+            placeholder: 'Your Age'
+        },
+        {
+            name: 'password',
+            type: 'password',
+            placeholder: 'Password'
+        },
+        {
+            name: 'password_repeat',
+            type: 'password',
+            placeholder: 'Repeat Password'
+        },
+        {
+            name: 'submit',
+            type: 'submit'
+        }
+    ];
 
-	inputs.forEach(function (item) {
-		const input = document.createElement('input');
+    inputs.forEach(function (item) {
+        const input = document.createElement('input');
 
-		input.name = item.name;
-		input.type = item.type;
+        input.name = item.name;
+        input.type = item.type;
 
-		input.placeholder = item.placeholder;
+        input.placeholder = item.placeholder;
 
-		form.appendChild(input);
-		form.appendChild(document.createElement('br'));
-	});
+        form.appendChild(input);
+        form.appendChild(document.createElement('br'));
+    });
 
-	signUpSection.appendChild(header);
-	signUpSection.appendChild(form);
-	signUpSection.appendChild(createMenuLink());
+    signUpSection.appendChild(header);
+    signUpSection.appendChild(form);
+    signUpSection.appendChild(createMenuLink());
 
-	form.addEventListener('submit', function (event) {
-		event.preventDefault();
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
 
-		const email = form.elements[ 'email' ].value;
-		const age = parseInt(form.elements[ 'age' ].value);
-		const password = form.elements[ 'password' ].value;
-		const password_repeat = form.elements[ 'password_repeat' ].value;
+        const email = form.elements['email'].value;
+        const age = parseInt(form.elements['age'].value);
+        const password = form.elements['password'].value;
+        const password_repeat = form.elements['password_repeat'].value;
+        //validation block
 
-		if (password !== password_repeat) {
-			alert('Passwords is not equals');
+        if (password.length < 4) {
 
-			return;
-		}
+            if (document.getElementById('err') !== null) {
+                const el = document.getElementById('err');
+                el.parentNode.removeChild(el)
+            }
+            const err = document.createElement('div');
+            err.setAttribute("id", "err");
 
-		ajax(function (xhr) {
-			game.innerHTML = '';
-			createProfile();
-		}, 'POST', '/signup', {
-			email: email,
-			age: age,
-			password: password
-		});
-	});
+            form.appendChild(err);
 
-	game.appendChild(signUpSection);
+            const att = document.createElement('p');
+            att.textContent = 'password must be at least 4 characters';
+            //if(document.getElementById('att')==false;)
+            err.appendChild(att);
+            return;
+        }
+        if (age < 0) {
+            if (document.getElementById('err') !== null) {
+                const el = document.getElementById('err');
+                el.parentNode.removeChild(el);
+            }
+            const err = document.createElement('div');
+            err.setAttribute("id", "err")
+
+            form.appendChild(err);
+            const att2 = document.createElement('p');
+            att2.textContent = 'age cannot be negative';
+            err.appendChild(att2);
+            return;
+        }
+        if (password !== password_repeat) {
+            alert('Passwords is not equals');
+
+            return;
+        }
+
+        ajax(function (xhr) {
+            game.innerHTML = '';
+            createProfile();
+        }, 'POST', '/signup', {
+            email: email,
+            age: age,
+            password: password
+        });
+    });
+
+    game.appendChild(signUpSection);
 }
 
-function createLeaderboard (users) {
-	const leaderboardSection = document.createElement('section');
-	leaderboardSection.dataset.sectionName = 'leaderboard';
+function createLeaderboard(users) {
+    const leaderboardSection = document.createElement('section');
+    leaderboardSection.dataset.sectionName = 'leaderboard';
 
-	const header = document.createElement('h1');
-	header.textContent = 'Leaders';
+    const header = document.createElement('h1');
+    header.textContent = 'Leaders';
 
-	leaderboardSection.appendChild(header);
-	leaderboardSection.appendChild(createMenuLink());
-	leaderboardSection.appendChild(document.createElement('br'));
+    leaderboardSection.appendChild(header);
+    leaderboardSection.appendChild(createMenuLink());
+    leaderboardSection.appendChild(document.createElement('br'));
 
-	if (users) {
-		const table = document.createElement('table');
-		const thead = document.createElement('thead');
-		thead.innerHTML = `
+    if (users) {
+        const table = document.createElement('table');
+        const thead = document.createElement('thead');
+        thead.innerHTML = `
 		<tr>
 			<th>Email</th>
 			<th>Age</th>
 			<th>Score</th>
 		</th>
 		`;
-		const tbody = document.createElement('tbody');
+        const tbody = document.createElement('tbody');
 
-		table.appendChild(thead);
-		table.appendChild(tbody);
-		table.border = 1;
-		table.cellSpacing = table.cellPadding = 0;
+        table.appendChild(thead);
+        table.appendChild(tbody);
+        table.border = 1;
+        table.cellSpacing = table.cellPadding = 0;
 
-		users.forEach(function (user) {
-			const email = user.email;
-			const age = user.age;
-			const score = user.score;
+        users.forEach(function (user) {
+            const email = user.email;
+            const age = user.age;
+            const score = user.score;
 
-			const tr = document.createElement('tr');
-			const tdEmail = document.createElement('td');
-			const tdAge = document.createElement('td');
-			const tdScore = document.createElement('td');
+            const tr = document.createElement('tr');
+            const tdEmail = document.createElement('td');
+            const tdAge = document.createElement('td');
+            const tdScore = document.createElement('td');
 
-			tdEmail.textContent = email;
-			tdAge.textContent = age;
-			tdScore.textContent = score;
+            tdEmail.textContent = email;
+            tdAge.textContent = age;
+            tdScore.textContent = score;
 
-			tr.appendChild(tdEmail);
-			tr.appendChild(tdAge);
-			tr.appendChild(tdScore);
+            tr.appendChild(tdEmail);
+            tr.appendChild(tdAge);
+            tr.appendChild(tdScore);
 
-			tbody.appendChild(tr);
+            tbody.appendChild(tr);
 
-			leaderboardSection.appendChild(table);
-		});
-	} else {
-		const em = document.createElement('em');
-		em.textContent = 'Loading';
-		leaderboardSection.appendChild(em);
+            leaderboardSection.appendChild(table);
+        });
+    } else {
+        const em = document.createElement('em');
+        em.textContent = 'Loading';
+        leaderboardSection.appendChild(em);
 
-		ajax(function (xhr) {
-			const users = JSON.parse(xhr.responseText);
-			game.innerHTML = '';
-			createLeaderboard(users);
-		}, 'GET', '/users');
-	}
+        ajax(function (xhr) {
+            const users = JSON.parse(xhr.responseText);
+            game.innerHTML = '';
+            createLeaderboard(users);
+        }, 'GET', '/users');
+    }
 
-	game.appendChild(leaderboardSection);
+    game.appendChild(leaderboardSection);
 }
 
-function createProfile (me) {
-	const profileSection = document.createElement('section');
-	profileSection.dataset.sectionName = 'profile';
+function createProfile(me) {
+    const profileSection = document.createElement('section');
+    profileSection.dataset.sectionName = 'profile';
 
-	const header = document.createElement('h1');
-	header.textContent = 'Profile';
+    const header = document.createElement('h1');
+    header.textContent = 'Profile';
 
-	profileSection.appendChild(header);
-	profileSection.appendChild(createMenuLink());
+    profileSection.appendChild(header);
+    profileSection.appendChild(createMenuLink());
 
-	if (me) {
-		const p = document.createElement('p');
+    if (me) {
+        const p = document.createElement('p');
 
-		const div1 = document.createElement('div');
-		div1.textContent = `Email ${me.email}`;
-		const div2 = document.createElement('div');
-		div2.textContent = `Age ${me.age}`;
-		const div3 = document.createElement('div');
-		div3.textContent = `Score ${me.score}`;
+        const div1 = document.createElement('div');
+        div1.textContent = `Email ${me.email}`;
+        const div2 = document.createElement('div');
+        div2.textContent = `Age ${me.age}`;
+        const div3 = document.createElement('div');
+        div3.textContent = `Score ${me.score}`;
 
-		p.appendChild(div1);
-		p.appendChild(div3);
-		p.appendChild(div3);
+        p.appendChild(div1);
+        p.appendChild(div3);
+        p.appendChild(div3);
 
-		profileSection.appendChild(p);
-	} else {
-		ajax(function (xhr) {
-			if (!xhr.responseText) {
-				alert('Unauthorized');
-				game.innerHTML = '';
-				createMenu();
-				return;
-			}
+        profileSection.appendChild(p);
+    } else {
+        ajax(function (xhr) {
+            if (!xhr.responseText) {
+                alert('Unauthorized');
+                game.innerHTML = '';
+                createMenu();
+                return;
+            }
 
-			const user = JSON.parse(xhr.responseText);
-			game.innerHTML = '';
-			createProfile(user);
-		}, 'GET', '/me');
-	}
+            const user = JSON.parse(xhr.responseText);
+            game.innerHTML = '';
+            createProfile(user);
+        }, 'GET', '/me');
+    }
 
-	game.appendChild(profileSection);
+    game.appendChild(profileSection);
 }
 
 const pages = {
-	menu: createMenu,
-	sign_in: createSignIn,
-	sign_up: createSignUp,
-	leaders: createLeaderboard,
-	me: createProfile
+    menu: createMenu,
+    sign_in: createSignIn,
+    sign_up: createSignUp,
+    leaders: createLeaderboard,
+    me: createProfile
 };
 
 createMenu();
 
 game.addEventListener('click', function (event) {
-	if (!(event.target instanceof HTMLAnchorElement)) {
-		return;
-	}
+    if (!(event.target instanceof HTMLAnchorElement)) {
+        return;
+    }
 
-	event.preventDefault();
-	const link = event.target;
+    event.preventDefault();
+    const link = event.target;
 
-	console.log({
-		href: link.href,
-		dataHref: link.dataset.href
-	});
+    console.log({
+        href: link.href,
+        dataHref: link.dataset.href
+    });
 
-	game.innerHTML = '';
+    game.innerHTML = '';
 
-	pages[ link.dataset.href ]();
+    pages[link.dataset.href]();
 });
