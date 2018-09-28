8c8b6ac40e453ae3ff0832d8b04a6198d26c255f
makdenis <makdenis.1997@gmail.com>
2018-09-29 01:25:03 +0300

    now onli static sharing

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-var app=require('express')();
-var http=require('http').Server(app);
-app.get('/', (req,res)=>{res.sendFile(__dirname+'/static/anothergame/html/index.html')});
-app.get('/about', (req,res)=>{res.sendFile(__dirname+'/static/anothergame/html/about.html')});
-app.get('/menu', (req,res)=>{res.sendFile(__dirname+'/static/anothergame/html/menu.html')});
-app.get('/profile', (req,res)=>{res.sendFile(__dirname+'/static/anothergame/html/profile.html')});
-app.get('/rating', (req,res)=>{res.sendFile(__dirname+'/static/anothergame/html/rating.html')});
-app.get('*', (req,res)=>{res.status('404').send('what?')});
-http.listen(3000, ()=>{
-console.log('listen 3000');})
+'use strict';
+
+const express = require('express');
+const body = require('body-parser');
+const cookie = require('cookie-parser');
+const morgan = require('morgan');
+const uuid = require('uuid/v4');
+const path = require('path');
+const app = express();
+const proxy = require('express-http-proxy');
+app.use(morgan('dev'));
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
+app.use(body.json());
+app.use(cookie());
+
+const port = process.env.PORT || 3000;
+//  app.use('*', proxy('https://lolkek-tssjobwzfy.now.sh/', {
+//     proxyReqPathResolver: function (req) {
+//         return req.originalUrl;
+//    }}));
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+app.listen(port, function () {
+    console.log(`Server listening port ${port}`);
+});
+
+
+
