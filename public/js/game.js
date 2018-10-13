35076bc087a2e5aaa6f889394420b8a8cc1982d5
makdenis <makdenis.1997@gmail.com>
2018-10-13 23:51:53 +0300

    validation

--- public/js/game.js
+++ public/js/game.js
@@ -168,6 +168,54 @@ function createSignUp() {
         .append(header)
         .append(createMenuLink())
         .append(form);
+game.append(signUpSection);
+    const check = document.getElementsByName("password")[0]
+
+    const check2 = document.getElementsByName("password_repeat")[0]
+    check.addEventListener("keyup", ()=>{
+        if (check.value.length<4){
+            check.setAttribute("class", "error")
+			if (document.getElementById('err') !== null) {
+				const el = document.getElementById('err');
+				el.parentNode.removeChild(el)
+			}
+
+			const err = Block.Create('div', {'id': 'err'}, []);
+			form.append(err);
+
+			const att = Block.Create('p', {}, [], 'password must be at least 4 characters');
+			err.append(att);
+        }
+        else{
+            check.setAttribute("class", "ok")
+			const el = document.getElementById('err');
+			el.parentNode.removeChild(el)
+        }
+
+    });
+    check2.addEventListener("keyup", ()=>{
+
+        if(check2.value!==check.value){
+			if (document.getElementById('err') !== null) {
+				const el = document.getElementById('err');
+				el.parentNode.removeChild(el)
+			}
+			check2.setAttribute("class", "error")
+			const err = Block.Create('div', {'id': 'err'}, []);
+			form.append(err);
+
+			const att = Block.Create('p', {}, [], 'password must be equal');
+			err.append(att);
+        }
+        else{
+			check2.setAttribute("class", "ok")
+			const el = document.getElementById('err');
+			console.log(el)
+			el.parentNode.removeChild(el)
+		}
+
+    });
+    
 
     form.onSubmit(
         function (formdata) {
@@ -222,7 +270,7 @@ function createSignUp() {
         }
     );
 
-    game.append(signUpSection);
+
 }
 
 /**
