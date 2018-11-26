569156e8247b226707fa9e1c7e1a8a4bfc83b14f
makdenis <makdenis.1997@gmail.com>
2018-11-26 16:30:48 +0300

    touch

--- public/js/views/MultView.js
+++ public/js/views/MultView.js
@@ -19,8 +19,8 @@ export default class MultView extends BaseView {
 
 		var rand = 0
 		const address = ["https", "https:"].includes(location.protocol) ?
-			`wss://127.0.0.1:9090/ws` :
-			`ws://127.0.0.1:9090/ws`
+			`wss://127.0.0.1:8000/ws` :
+			`ws://127.0.0.1:8000/ws`
 
 
 		let ws = new WebSocket(address)
@@ -46,16 +46,8 @@ export default class MultView extends BaseView {
 			}
 		}
 
-		const logo = Block.Create("canvas", {
-			"id": "myCanvas",
-			"width": "1024",
-			"height": "768",
-
-		}, [])
-
-
-		this.el.append(logo)
-
+		const canv=Block.Create("canvas", {"id": "myCanvas" }, [])
+		this.el.append(canv)
 		const signIn = window.chatFields
 		const signInSection = Block.Create("section", {"data-section-name": "sign_in"}, [])
 		const header = Block.Create("h1", {}, [], "Чат")
@@ -80,28 +72,71 @@ export default class MultView extends BaseView {
 			}
 		)
 		let onload = 0
-		let canvas = document.getElementById("myCanvas")
-		canvas.style.background = "url('../../img/textures/4.png')"
+
+		let canvas
+		let ctx
+		let k1
+		let k2
+		let mousePos
+
+		canvas = document.getElementById('myCanvas');
+		let paddleHeight = 50
+		let paddleWidth = 50
+		let paddleX = (canvas.width - paddleWidth) / 2
+		let paddleY = (canvas.height) - 50
+		if (canvas.getContext) {
+			ctx = canvas.getContext("2d");
+
+
+			resizeCanvas();
+		}
+
+		function getTouchPos(canvasDom, touchEvent) {
+			var rect = canvasDom.getBoundingClientRect();
+			if(touchEvent.touches[0]!==undefined){
+				return {
+
+					x: touchEvent.touches[0].clientX - rect.left,
+					y: touchEvent.touches[0].clientY - rect.top}
+			}
+		}
+		window.addEventListener('resize', resizeCanvas, false);
+		window.addEventListener('orientationchange', resizeCanvas, false);
+
+
+		function resizeCanvas() {
+			k1=canvas.width / window.innerWidth;
+			k2=canvas.height / window.innerHeight;
+			if(k1===k2&&k1===0){
+				k1=1
+				k2=1
+			}
+
+			canvas.width = window.innerWidth;
+			canvas.height = window.innerHeight;
+			paddleY=paddleY/k2
+			paddleX=paddleX/k1
+		}
+
+
+
+
+		// let canvas = document.getElementById("myCanvas")
 		let car = new Image()
 		let enemy2 = new Image()
 		let enemy3 = new Image()
 		let enemy4 = new Image()
 		let enemy = new Image()
+		let enemy5 = new Image()
 		let enemy21 = new Image()
 		let enemy22 = new Image()
 		let enemy23 = new Image()
 		let enemy24 = new Image()
-		let enemy5 = new Image()
-		let img = ["../../img/textures/1.png", "../../img/textures/2.png", "../../img/textures/3.png", , "../../img/textures/5.png"]
-		let ctx = canvas.getContext("2d")
+		let img = ["../../img/textures/1.png", "../../img/textures/2.png", "../../img/textures/3.png", "../../img/textures/3.png", "../../img/textures/3.png"]
+
 		let background = new Image()
-		var msg = undefined
-		let paddleHeight = 50
-		let paddleWidth = 50
-		let xenemy = (canvas.width - paddleWidth) / 2
-		let yenemy = (canvas.height) - 50
-		let paddleX = (canvas.width - paddleWidth) / 2
-		let paddleY = (canvas.height) - 50
+
+
 		let x = 0
 		let x2 = canvas.width
 		let y = canvas.height - 300
@@ -113,8 +148,9 @@ export default class MultView extends BaseView {
 		let tick = 0
 		let level = 1
 		var msg2
-		var mes = null
-		let game = this.el
+		let msg=undefined
+		let xenemy = (canvas.width - paddleWidth) / 2
+		let yenemy = (canvas.height) - 50
 
 		function drawPaddle() {
 			ctx.drawImage(car, paddleX, paddleY)
@@ -161,7 +197,8 @@ export default class MultView extends BaseView {
 
 		function draw() {
 			ctx.clearRect(0, 0, canvas.width, canvas.height)
-
+			ctx.fillStyle="#000000";
+			ctx.fillRect(0,0,canvas.width,canvas.height);
 
 			ws.onmessage = function (msgevent) {
 				msg = JSON.parse(msgevent.data)
@@ -224,44 +261,38 @@ export default class MultView extends BaseView {
 			drawenemy()
 
 			if (((paddleX > x && paddleX < x + 60) || (paddleX > x - 200 && paddleX < x + 60 - 200) || (paddleX > x - 400 && paddleX < x + 60 - 400) || (paddleX > x - 600 && paddleX < x + 60 - 600)) && (paddleY < y + 60 && paddleY > y)) {
-                alert("Конец игры. Ваш счет - " + tick)
-                paddleX = (canvas.width - paddleWidth) / 2
-                paddleY = (canvas.height) - 50
-                tick = 0
-                upPressed=false
-                downPressed=false
-                leftPressed=false
-                rightPressed=false
-
-     //document.location.reload()
-            }
-            if (((paddleX > x2 && paddleX < x2 + 60) || (paddleX > x2 + 200 && paddleX < x2 + 60 + 200) || (paddleX > x2 + 400 && paddleX < x2 + 60 + 400) || (paddleX > x2 + 600 && paddleX < x2 + 60 + 600)) && (paddleY < y + 60 + 150 && paddleY > y + 150)) {
-                alert("Конец игры. Ваш счет - " + tick)
-                paddleX = (canvas.width - paddleWidth) / 2
-                paddleY = (canvas.height) - 50
-                tick=0
-upPressed=false
-                downPressed=false
-                leftPressed=false
-                rightPressed=false
-
-
-                //document.location.reload()
-                
-            }
-            if (((paddleX > x && paddleX < x + 60) || (paddleX > x - 200 && paddleX < x + 60 - 200) || (paddleX > x - 400 && paddleX < x + 60 - 400) || (paddleX > x - 600 && paddleX < x + 60 - 600)) && (paddleY < y + 60 - 250 && paddleY > y - 250)) {
-                alert("Конец игры. Ваш счет - " + tick)
-                paddleX = (canvas.width - paddleWidth) / 2
-                paddleY = (canvas.height) - 50
-                //document.location.reload()
-                tick=0
-                upPressed=false
-                downPressed=false
-                leftPressed=false
-                rightPressed=false
-
-
-            }
+				alert("Конец игры. Ваш счет - " + tick)
+				paddleX = (canvas.width - paddleWidth) / 2
+				paddleY = (canvas.height) - 50
+				tick = 0
+				leftPressed = false
+				rightPressed = false
+				upPressed = false
+				downPressed = false
+				level=0
+			}
+			if (((paddleX > x2 && paddleX < x2 + 60) || (paddleX > x2 + 200 && paddleX < x2 + 60 + 200) || (paddleX > x2 + 400 && paddleX < x2 + 60 + 400) || (paddleX > x2 + 600 && paddleX < x2 + 60 + 600)) && (paddleY < y + 60 + 150 && paddleY > y + 150)) {
+				alert("Конец игры. Ваш счет - " + tick)
+				paddleX = (canvas.width - paddleWidth) / 2
+				paddleY = (canvas.height) - 50
+				tick = 0
+				leftPressed = false
+				rightPressed = false
+				upPressed = false
+				downPressed = false
+				level=0
+			}
+			if (((paddleX > x && paddleX < x + 60) || (paddleX > x - 200 && paddleX < x + 60 - 200) || (paddleX > x - 400 && paddleX < x + 60 - 400) || (paddleX > x - 600 && paddleX < x + 60 - 600)) && (paddleY < y + 60 - 250 && paddleY > y - 250)) {
+				alert("Конец игры. Ваш счет - " + tick)
+				paddleX = (canvas.width - paddleWidth) / 2
+				paddleY = (canvas.height) - 50
+				tick = 0
+				leftPressed = false
+				rightPressed = false
+				upPressed = false
+				downPressed = false
+				level=0
+			}
 
 			if (rightPressed && paddleX < canvas.width - paddleWidth) {
 				paddleX += 1
@@ -292,7 +323,32 @@ upPressed=false
 
 		document.addEventListener("keydown", keyDownHandler, false)
 		document.addEventListener("keyup", keyUpHandler, false)
+		canvas.addEventListener("touchstart", function (a) {
+			mousePos = getTouchPos(canvas, a);
+			console.log(mousePos)
+			if(mousePos.x>0&&mousePos.x<30){
+				leftPressed=true
+			}
+			if(mousePos.x>canvas.width-30&&mousePos.x<canvas.width){
+				rightPressed=true
+			}
+			if(mousePos.y>canvas.height-30&&mousePos.y<canvas.height){
+				upPressed=true
+			}
+		})
+		canvas.addEventListener("touchend", function (a) {
+			mousePos = getTouchPos(canvas, a);
+			console.log(mousePos)
 
+			leftPressed=false
+
+
+			rightPressed=false
+
+
+			upPressed=false
+
+		})
 		function keyDownHandler(e) {
 			if (e.keyCode === 39) {
 				rightPressed = true
